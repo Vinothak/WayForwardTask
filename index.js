659d16e274143520fbaecc3a757c47707f8150a7
Vinothak <56429360+Vinothak@users.noreply.github.com>
2020-08-31 15:32:43 +0530

    i have uploaded my relevant files now.

---
+++ index.js
@@ -0,0 +1,17 @@
+function progress(){
+    var prog=document.getElementById('progress');
+    var percent=document.getElementById('percentCount');
+    var counter=5;
+    var progress=25;
+    var id=setInterval(()=>{
+      if(progress==500 && counter ==100){
+          clearInterval(id);
+      }else{
+          progress+=5;
+          counter+=1;
+          prog.style.width=progress+'px';
+          percent.innerHTML=counter+'%';
+      }
+    },50);
+}
+
